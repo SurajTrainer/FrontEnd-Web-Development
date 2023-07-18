287754d9bb20a72783321718c1c6abf8de4a17a4
Suraj <playerking2244@gmail.com>
2023-07-18 16:53:44 +0530

    Visual

---
+++ Visual Coding/first react/App.js
@@ -0,0 +1,15 @@
+import logo from './clock.jpg';
+
+import Subcription from './first react/Subcription';
+
+function App(){
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Subcription />
+                </header> 
+        </div> 
+    );
+}
+
+export default App;
---
+++ Visual Coding/first react/Subcription.js
@@ -0,0 +1,7 @@
+function Subcription(){
+    return (
+        <h2>Subcription Title</h2>
+
+    )
+}
+export default Subcription
